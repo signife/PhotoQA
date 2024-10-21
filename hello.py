82b856be142288085fed4bcc6f179c629ea4e71c
Hyunmin Oh <hyunmin@minicode.com.au>
2024-10-21 16:38:52 +1000

    PQA-10 add: function of fibonacci

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
+def fibonacci(n):
+    fib_sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    return fib_sequence
+
 def main():
-    print("Hello, Docker!")
+    n = int(input("put number: "))
+    fib_sequence = fibonacci(n)
+    print(f"Fibonacci: {fib_sequence}")
 
 if __name__ == "__main__":
     main()
