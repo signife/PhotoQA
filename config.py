6345c6c1f9adc7597712beb837d5cc6ba3440d4f
Hyunmin Oh <hyunmin@minicode.com.au>
2024-11-05 10:23:54 +1000

    PQA-17 : change Learning Late, and Epochs.

--- config.py
+++ config.py
@@ -1,19 +1,22 @@
 # Configuration settings
 
+import torch
 # Paths
 IMAGE_DIR = "data/cropdata/"  # Update this to the correct path
-CSV_FILE_PATH = 'data/data-1729656846556.csv'
+CSV_FILE_PATH = 'data/undersampled_images_data.csv'
 
 # DataLoader settings
 BATCH_SIZE = 32
 
 # Model settings
-MODEL_NAME = 'resnet18'  # You can change to other models like 'resnet34', 'resnet50' if needed
+# MODEL_NAME = 'resnet18'  # You can change to other models like 'resnet34', 'resnet50' if needed
 
 # Training settings
-NUM_EPOCHS = 5
-LEARNING_RATE = 0.01
+NUM_EPOCHS = 10
+LEARNING_RATE = 0.005
 
 # Normalization parameters for pretrained models
 NORMALIZE_MEAN = [0.485, 0.456, 0.406]
 NORMALIZE_STD = [0.229, 0.224, 0.225]
+
+DEVICE = 'cpu'
